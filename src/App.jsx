b3a12a1d5f1a5d9f6e9875838bf67fee294fe7cc
Riki Fameli <rikifameli@gmail.com>
2022-05-26 20:28:10 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import GlobalStyle from './globalStyles.styled'
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      Hello
+    </Fragment>
+  )
+}
+
+export default App
