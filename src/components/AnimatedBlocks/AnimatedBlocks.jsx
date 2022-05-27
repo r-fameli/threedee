401e85b8ae6e7d9cd45a6c96dd26f57f3a34c11a
Riki Fameli <rikifameli@gmail.com>
2022-05-27 14:53:24 -0400

    Complete Wrong Akram tutorial - three wobbly boxes!

--- src/components/AnimatedBlocks/AnimatedBlocks.jsx
+++ src/components/AnimatedBlocks/AnimatedBlocks.jsx
@@ -1,12 +1,45 @@
-import { Canvas } from "react-three-fiber";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { softShadows, OrbitControls } from "@react-three/drei";
+
+import SpinningMesh from "../SpinningMesh/SpinningMesh";
+
+softShadows();
 
 export default function AnimatedBlocks() {
     return (
-        <Canvas>
-            <mesh>
-            <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
-            <meshStandardMaterial attach='material'/>
-            </mesh>
+        <Canvas shadows colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+            <ambientLight intensity={0.7}/>
+            <directionalLight
+                castShadow
+                position={[0, 10, 0]}
+                intensity={1.5}
+                shadow-mapSize-width={10240}
+                shadow-mapSize-height={10240}
+                shadow-camera-far={20}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+            <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+            <pointLight position={[0, -10, 0]} intensity={0.5}/>
+
+            <group>
+                <mesh
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    position={[0, -3, 0]}
+                    receiveShadow>
+                    <planeBufferGeometry attach='geometry' args={[100, 100]} />
+                    <shadowMaterial attach='material' opacity={0.3} />
+                </mesh>
+                <SpinningMesh position={[0,1,0]} args={[3,2,1]} color='lightblue' speed={2}/>
+                <SpinningMesh position={[-2,1,-5]} color='pink' speed={6}/>
+                <SpinningMesh position={[5,1,-2]} color='pink' speed={6}/>
+            </group>
+
+            
+            <OrbitControls/>
         </Canvas>
     )
 }
---
+++ src/components/SpinningMesh/SpinningMesh.jsx
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { MeshWobbleMaterial } from "@react-three/drei";
+import { useSpring, animated } from "@react-spring/three";
+
+
+export default function SpinningMesh({ position, args, color, speed }) {
+    const mesh = useRef(null);
+    useFrame(() => {
+        const rotationLevel = 0.01
+        mesh.current.rotation.x += rotationLevel
+        mesh.current.rotation.y += rotationLevel
+    })
+
+    const [expand, setExpand] = useState(false);
+    const props = useSpring({
+        scale: expand ? [1.4, 1.4, 1.4] : [1,1,1]
+    })
+
+    return (
+        <animated.mesh 
+            onClick={() => setExpand(!expand)}
+            scale={props.scale}
+            castShadow 
+            position={position} 
+            ref={mesh}
+        >
+            <boxBufferGeometry attach='geometry' args={args}/>
+            {/* <meshStandardMaterial attach='material' color={color} /> */}
+            <MeshWobbleMaterial 
+                attach='material' 
+                color={color} 
+                speed={speed} 
+                factor={0.5}
+            />
+        </animated.mesh>
+    )
+}
